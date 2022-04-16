41e5ba29b1f4abe5197b767e27eab472f9157da7
owati <mofiyinfoluwaowati@gmail.com>
2022-04-16 10:35:40 +0100

    working on sockets connection

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,71 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const express_ws_1 = __importDefault(require("express-ws"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
+const redis_1 = require("redis");
+dotenv_1.default.config();
 const wsInstance = (0, express_ws_1.default)((0, express_1.default)());
 const app = wsInstance.app;
 const router = express_1.default.Router();
+const wss = wsInstance.getWss();
+const client = (0, redis_1.createClient)();
+client.connect(); // connect to redis server
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
+app.use((0, cors_1.default)({
+    origin: ''
+}));
+function generateClientId() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield client.get('user');
+        if (typeof user === 'string') {
+            let userArray = JSON.parse(user);
+            return userArray.length;
+        }
+    });
+}
+app.use((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    client.on('error', err => {
+        console.log(err.message);
+    });
+    let games = yield client.get('games');
+    let users = yield client.get('user');
+    if (!(games && users)) { // creates the game and user redis objects if they do not yet exist
+        yield client.set('user', '[]'); // sets the user space 
+        yield client.set('games', "[]"); // sets the game space
+    }
+    next();
+}));
 app.get('/', (req, res) => {
     console.log(req.headers);
     res.send('hello world');
 });
-wsInstance.getWss().on('connection', function connection(ws) {
-    console.log(wsInstance.getWss().clients);
+wss.on('connection', function connection(ws, req) {
+    // console.log(wsInstance.getWss().clients)
     ws.send('welcome');
 });
+wss.on('message', function connection(ws, req) {
+    console.log(ws);
+});
 router.ws('/home', (ws, req) => {
-    ws.on('message', () => {
-        console.log(wsInstance.getWss().clients);
-    });
+    ws.onmessage = (e) => {
+        console.log(e);
+    };
 });
 app.listen('3001', () => {
     console.log('node server has started...');
--- index.ts
+++ index.ts
@@ -1,26 +1,72 @@
-import express , {Express, Request, Response} from 'express';
+import dotenv from 'dotenv';
+import express , {Express, Request, Response, NextFunction} from 'express';
 import expressWs, {Application, Instance, Router} from 'express-ws';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import redis, {RedisClientType, createClient} from 'redis';
+
+dotenv.config();
 
 const wsInstance : Instance = expressWs(express())
 const app : Application = wsInstance.app;
 const router : Router  = express.Router();
+const wss = wsInstance.getWss();
+
+const client : RedisClientType = createClient()
+client.connect(); // connect to redis server
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json())
+app.use(cors({
+    origin : ''
+}))
+
+async function generateClientId() {
+    const user: string | null = await client.get('user');
+    if (typeof user === 'string') {
+        let userArray : Array<string> = JSON.parse(user);
+        return userArray.length;
+    }
+
+}
+
+app.use(async (req : Request , res : Response, next: NextFunction) => { // connects to redis
+    client.on('error', err => {
+        console.log(err.message);
+    })
+    let games = await  client.get('games');
+    let users = await client.get('user')
+    if (!(games && users)) {  // creates the game and user redis objects if they do not yet exist
+        await client.set('user', '[]'); // sets the user space 
+        await client.set('games', "[]"); // sets the game space
+
+    } 
+
+    next();
+})
 
 
-app.get('/',  (req : Request, res : Response) => {
+app.get('/',  (req : Request, res : Response) => {  /// remove later
     console.log(req.headers)
     res.send('hello world')
 })
 
-wsInstance.getWss().on('connection', function connection(ws) {
 
-    console.log(wsInstance.getWss().clients)
+wss.on('connection', function connection(ws, req) { // 
+
+   // console.log(wsInstance.getWss().clients)
+   
     ws.send('welcome')
+})
+wss.on('message', function connection(ws, req) { // 
+    console.log(ws)
 })
 
 router.ws('/home', (ws, req : Request) => {
-    ws.on('message', () => {
-        console.log(wsInstance.getWss().clients)
-    })
+    
+    ws.onmessage = (e) => {
+        console.log(e)
+    }
 })
 
 
