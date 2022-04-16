0e25745f102886443b9a367151dcda27a5302a29
owati <mofiyinfoluwaowati@gmail.com>
2022-04-16 20:55:26 +0100

    started the lobby socket endpoint

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -22,10 +22,45 @@ const redis_client = (0, redis_1.createClient)();
 const lobbySocket = new ws_1.WebSocketServer({ noServer: true }); // server for the game lobby
 const gameSocket = new ws_1.WebSocketServer({ noServer: true }); // server for the game action
 lobbySocket.on('connection', (ws, req) => __awaiter(void 0, void 0, void 0, function* () {
+    const games_json = yield redis_client.get('game');
+    const user_json = yield redis_client.get('users');
+    const games = JSON.parse(String(games_json));
+    const users = JSON.parse(String(user_json));
+    if (!ws.id) { // the client is recognised by the server
+        ws.id = yield generateId();
+    }
+    ws.send(JSON.stringify({
+        games: games
+    }));
+    for (let client of lobbySocket.clients) { // send the new number of users to all clients
+        client.send(JSON.stringify({
+            users: users.length
+        }));
+    }
     ws.on('message', (message) => __awaiter(void 0, void 0, void 0, function* () {
-        console.log(message.toString(), ws);
+        const data = JSON.parse(message.toString());
+        console.log(data, games, users);
     }));
 }));
+gameSocket.on('connection', (ws, req) => __awaiter(void 0, void 0, void 0, function* () {
+    ws.on('message', (message) => __awaiter(void 0, void 0, void 0, function* () {
+        const data = JSON.parse(message.toString());
+        console.log(data);
+    }));
+}));
+function generateId() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const users_json = yield redis_client.get('users');
+        const users = JSON.parse(String(users_json));
+        let num;
+        do {
+            num = Math.random() * 10000;
+        } while (users.includes(num)); // ensures the number is not in the user array
+        users.push(num); // adds the number to the array
+        yield redis_client.set('users', JSON.stringify(users)); // sets the number back in the redis object
+        return num;
+    });
+}
 const server = app.listen(process.env.PORT, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log('node server has started');
     yield redis_client.connect();
--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
+'use strict';
+
 import dotenv from 'dotenv';
 import express , {Express, Request, Response, NextFunction} from 'express';
-
 import { request, Server, IncomingMessage} from 'http';
-import {WebSocketServer} from 'ws';
+import {WebSocketServer, WebSocket} from 'ws';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import redis, {RedisClientType, createClient} from 'redis';
 
+interface myWebSocket extends WebSocket {
+    id?: number  // adding id property for the web socket
+}
 
 dotenv.config();
 
@@ -18,11 +22,54 @@ const redis_client : RedisClientType = createClient();
 const lobbySocket : WebSocketServer = new WebSocketServer({noServer : true}) // server for the game lobby
 const gameSocket : WebSocketServer  = new WebSocketServer({noServer : true})  // server for the game action
 
-lobbySocket.on('connection', async (ws, req) => {
+lobbySocket.on('connection', async (ws : myWebSocket, req) => {
+    const games_json  = await redis_client.get('game');
+    const user_json = await redis_client.get('users');
+    const games = JSON.parse(String(games_json));
+    const users = JSON.parse(String(user_json));
+
+
+    if(!ws.id) { // the client is recognised by the server
+        ws.id =  await generateId()
+    }
+
+    ws.send(JSON.stringify({ // sends the available games to the user
+        games : games
+    }));
+
+    for(let client of lobbySocket.clients) { // send the new number of users to all clients
+        client.send(JSON.stringify({
+            users : users.length
+        }));
+    }
+
     ws.on('message', async (message) => {
-        console.log(message.toString(), ws)
+        const data = JSON.parse(message.toString());
+        console.log(data, games, users );
     })
 })
+
+gameSocket.on('connection', async (ws, req) =>  {
+    ws.on('message', async (message) => {
+        const data = JSON.parse(message.toString());
+        console.log(data);
+    } )
+})
+
+
+async function generateId () : Promise<number> {
+    const users_json = await redis_client.get('users');
+    const users = JSON.parse(String(users_json));
+    let num : number;
+    do {
+        num = Math.random() * 10000;
+    } while(users.includes(num));  // ensures the number is not in the user array
+
+    users.push(num); // adds the number to the array
+    await redis_client.set('users', JSON.stringify(users)); // sets the number back in the redis object
+
+    return num;
+}
 
 
 
