347cc1dc27f6a1aa99d1781f2a439fea474a6e65
owati <mofiyinfoluwaowati@gmail.com>
2022-05-02 14:50:43 +0100

    worked the gae

--- dist/index.js
+++ dist/index.js
@@ -16,6 +16,46 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const ws_1 = require("ws");
 const redis_1 = require("redis");
+class Game {
+    constructor(game) {
+        this.game = game;
+    }
+    getPlayers() {
+        var _a, _b;
+        return [(_a = this.game) === null || _a === void 0 ? void 0 : _a.player1, (_b = this.game) === null || _b === void 0 ? void 0 : _b.player2];
+    }
+    setPlayer(id) {
+        var _a, _b;
+        if ((_a = this.game) === null || _a === void 0 ? void 0 : _a.player1) {
+            if ((_b = this.game) === null || _b === void 0 ? void 0 : _b.player2) {
+                return false;
+            }
+            else {
+                this.game.player2 = id;
+            }
+        }
+        else {
+            this.game.player1 = id;
+        }
+        return true;
+    }
+    refresh() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const games = JSON.parse(String(yield redis_client.get('game')));
+            for (let game of games) {
+                if (game.id === this.game.id) {
+                    games[games.indexOf(game)] = this.game;
+                    break;
+                }
+            }
+            console.log(games, 3);
+            yield redis_client.set('game', JSON.stringify(games));
+        });
+    }
+    getGame() {
+        return this.game;
+    }
+}
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const redis_client = (0, redis_1.createClient)();
@@ -51,19 +91,34 @@ gameSocket.on('connection', (ws, req) => __awaiter(void 0, void 0, void 0, funct
     var _a;
     console.log(req.url);
     const game_id = (_a = req.url) === null || _a === void 0 ? void 0 : _a.split('/')[2];
-    const games = JSON.parse(String(yield redis_client.get('games')));
-    if ((() => {
-        let exists = false;
-        for (const { id } of games) {
-            if (id == games_id) {
-                exists = true;
-                break;
+    const games = JSON.parse(String(yield redis_client.get('game')));
+    const game = new Game((() => {
+        for (const game of games) {
+            if (game.id == game_id) {
+                return game;
             }
         }
-        return exists;
-    })()) {
-        if (!ws.id) {
-            ws.id = yield generateId(); // give id to the player
+        return null;
+    })());
+    console.log(game);
+    if (game) {
+        if (game.getPlayers().includes(null)) {
+            if (!ws.id) {
+                ws.id = yield generateId(); // give id to the player
+            }
+            if (game.setPlayer(ws.id)) {
+                yield game.refresh();
+                ws.send(JSON.stringify({
+                    type: "game entrance",
+                    game: game.getGame()
+                }));
+            }
+        }
+        else {
+            ws.send(JSON.stringify({
+                type: "full game"
+            }));
+            ws.close();
         }
     }
     ws.on('message', (message) => __awaiter(void 0, void 0, void 0, function* () {
--- index.ts
+++ index.ts
@@ -1,51 +1,92 @@
 'use strict';
 
 import dotenv from 'dotenv';
-import express , {Express, Request, Response, NextFunction} from 'express';
-import { request, Server, IncomingMessage} from 'http';
-import {WebSocketServer, WebSocket} from 'ws';
+import express, { Express, Request, Response, NextFunction } from 'express';
+import { request, Server, IncomingMessage } from 'http';
+import { WebSocketServer, WebSocket } from 'ws';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import redis, {RedisClientType, createClient} from 'redis';
+import redis, { RedisClientType, createClient } from 'redis';
 
 interface myWebSocket extends WebSocket {
     id?: number  // adding id property for the web socket
 }
 
 interface GameObject {
-    name : string,
-    id : number,
-    player1 : number | null,
-    player2 : number | null,
-    games_status : Array<string>
+    name: string,
+    id: number,
+    player1: number | null,
+    player2: number | null,
+    games_status: Array<string>
+}
+
+class Game {
+    constructor(public game: GameObject) {
+    }
+
+    getPlayers() {
+        return [this.game?.player1, this.game?.player2]
+    }
+
+    setPlayer(id: number) {
+        if (this.game?.player1) {
+            if (this.game?.player2) {
+                return false
+            } else {
+                this.game.player2 = id
+            }
+        } else {
+            this.game.player1 = id;
+        }
+        return true
+    }
+
+    async refresh() {
+        const games = JSON.parse(String(await redis_client.get('game')));
+
+        for (let game of games) {
+            if (game.id === this.game.id) {
+                games[games.indexOf(game)] = this.game;
+                break;
+            }
+        }
+
+        console.log(games, 3)
+
+        await redis_client.set('game', JSON.stringify(games));
+    }
+
+    getGame() {
+        return this.game
+    }
 }
 
 dotenv.config();
 
-const app : Express = express();
+const app: Express = express();
 
-const redis_client : RedisClientType = createClient();
+const redis_client: RedisClientType = createClient();
 
 
-const lobbySocket : WebSocketServer = new WebSocketServer({noServer : true}) // server for the game lobby
-const gameSocket : WebSocketServer  = new WebSocketServer({noServer : true})  // server for the game action
+const lobbySocket: WebSocketServer = new WebSocketServer({ noServer: true }) // server for the game lobby
+const gameSocket: WebSocketServer = new WebSocketServer({ noServer: true })  // server for the game action
 
 lobbySocket.on('connection', async (ws, req) => {
 
     ws.on('message', async (message) => { // handle the creation of new games
         console.log(message.toString());
         const data = JSON.parse(message.toString());
-        const games = JSON.parse(String( await redis_client.get('game')));
+        const games = JSON.parse(String(await redis_client.get('game')));
 
-        
-        if(data.type === 'game create') {
+
+        if (data.type === 'game create') {
             console.log('creating a new game.................')
-            let game_object : GameObject = { // creates a new game object
-                name : data.name,
-                id : await generateGameId(),
-                player1 : null, 
-                player2 : null,
-                games_status : ['','','','','','','','','']   
+            let game_object: GameObject = { // creates a new game object
+                name: data.name,
+                id: await generateGameId(),
+                player1: null,
+                player2: null,
+                games_status: ['', '', '', '', '', '', '', '', '']
             }
 
             games.push(game_object);
@@ -53,11 +94,11 @@ lobbySocket.on('connection', async (ws, req) => {
             await redis_client.set('game', JSON.stringify(games));
 
             ws.send(JSON.stringify({
-                type : "new game",
-                id : game_object.id
+                type: "new game",
+                id: game_object.id
             }))
         }
-        
+
     })
 
     ws.on('close', async () => {  // the user id from the user array...
@@ -65,40 +106,58 @@ lobbySocket.on('connection', async (ws, req) => {
     })
 })
 
-gameSocket.on('connection', async (ws : myWebSocket, req) =>  {
+gameSocket.on('connection', async (ws: myWebSocket, req) => {
     console.log(req.url)
     const game_id = req.url?.split('/')[2]
-    const games = JSON.parse(String(await redis_client.get('games')))
+    const games = JSON.parse(String(await redis_client.get('game')))
 
-    if ((() : boolean => { // checks of the if is a valid id
-        let exists : boolean = false
-        for (const {id} of games) {
-            if(id == games_id) {
-                exists = true;
-                break;
+
+    const game = new Game(((): any => { // checks if the id is valid and returns the game with that id
+        for (const game of games) {
+            if (game.id == game_id) {
+                return game;
+
             }
         }
-        return exists
-    })()) {
-        if (!ws.id) {
-            ws.id = await generateId() // give id to the player
+        return null;
+    })())
+
+    console.log(game)
+
+    if (game) {
+        if (game.getPlayers().includes(null)) {
+            if (!ws.id) {
+                ws.id = await generateId() // give id to the player
+            }
+            if(game.setPlayer(ws.id)){
+                await game.refresh();
+                ws.send(JSON.stringify({
+                    type : "game entrance",
+                    game : game.getGame()
+                }))
+            }
+        } else {
+            ws.send(JSON.stringify({
+                type: "full game"
+            }));
+            ws.close();
         }
     }
     ws.on('message', async (message) => {
         const data = JSON.parse(message.toString());
         console.log(data);
 
-    } )
+    })
 })
 
 
-async function generateId () : Promise<number> {
+async function generateId(): Promise<number> {
     const users_json = await redis_client.get('users');
     const users = JSON.parse(String(users_json));
-    let num : number;
+    let num: number;
     do {
         num = Math.floor(Math.random() * 10000);
-    } while(users.includes(num));  // ensures the number is not in the user array
+    } while (users.includes(num));  // ensures the number is not in the user array
 
     users.push(num); // adds the number to the array
     await redis_client.set('users', JSON.stringify(users)); // sets the number back in the redis object
@@ -106,15 +165,15 @@ async function generateId () : Promise<number> {
     return num;
 }
 
-async function generateGameId() : Promise<number> {
+async function generateGameId(): Promise<number> {
     const games = JSON.parse(String(await redis_client.get('game')));
-    let num : number;
+    let num: number;
     do {
         num = Math.floor(Math.random() * 10000);
-    } while((() : boolean => {
-        let check : boolean = false;
-        for (let {id} of games) {
-            if(id == num)  {
+    } while (((): boolean => {
+        let check: boolean = false;
+        for (let { id } of games) {
+            if (id == num) {
                 check = true;
                 break;
             }
@@ -128,7 +187,7 @@ async function generateGameId() : Promise<number> {
 
 
 
-const server : Server = app.listen(process.env.PORT, async () => {
+const server: Server = app.listen(process.env.PORT, async () => {
     console.log('node server has started')
     await redis_client.connect();
     const game = await redis_client.get('game');  // get the game data from redis
@@ -137,14 +196,14 @@ const server : Server = app.listen(process.env.PORT, async () => {
     if (!(game && users)) {   // if the both of them do not exits 
         await redis_client.set('game', '[]');
         await redis_client.set('users', '[]')
-    }   
+    }
     console.log('redis server connected');
 });
 
-server.on('upgrade', (request : IncomingMessage, socket, head) => {
-    const pathname  : any = request.url;
+server.on('upgrade', (request: IncomingMessage, socket, head) => {
+    const pathname: any = request.url;
 
-    if (pathname === '/lobby' ) {
+    if (pathname === '/lobby') {
         lobbySocket.handleUpgrade(request, socket, head, ws => {
             lobbySocket.emit('connection', ws, request);
         });
